7305b654de63b3d945f134e4368bf827d901b169
Chulhwa Han <cjfghk5697@ajou.ac.kr>
2022-11-20 14:10:20 +0000

    delete file

--- Vision Models/GoogleNet/visualization.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-def show(img,y=None):
-    npimg=img.numpy()
-    npimg_tr=np.transpose(npimg,(1,2,0))
-    plt.imshow(npimg_tr)
-    if y is not None:
-        plt.title('lables : '+str(y))
-
